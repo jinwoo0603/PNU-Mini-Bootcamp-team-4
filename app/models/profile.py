51a8ba9a5c0f6c0a11b6ed56d551e2f5a6d1eb22
RyuTaeuk <ryuty77@naver.com>
2025-02-09 22:02:22 +0900

    added profile feature(draft)

---
+++ app/models/profile.py
@@ -0,0 +1,23 @@
+
+#로그인이 이미 되었다 가정하고 메인화면에서 구현되어야 할 기능을 구현하기
+#프로필의 구성요소: 프로필 사진, 아이디(닉네임), 팔로잉, 팔로워, 바이오
+#디비에서 아이디를 가져와야 될듯
+from sqlmodel import Field
+from typing import Optional
+from pydantic import BaseModel
+
+#Profile Request(input)
+class CreateProfileReq(BaseModel):
+    username: str
+    bio: Optional[str] = None
+    published: bool = Field(index=True)
+
+class ProfileResp(BaseModel):
+    pass
+
+class Profile(BaseModel):
+    user_id: int = Field(primary_key=True)
+    username: str
+    bio: Optional[str] = None
+    published: bool = Field(index=True)
+
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 from app.dependencies.db import create_db
+from contextlib import asynccontextmanager
+from app.routers import profile_handler
 
-app = FastAPI()
+db_url = 'sqlite:///blog.db'
 
-@app.on_event('startup')
-def on_startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     create_db()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(profile_handler.router)
