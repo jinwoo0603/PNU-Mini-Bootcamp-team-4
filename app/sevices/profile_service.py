b4224550cc81cb01ce5791e318519d3fdbd3c934
RyuTaeuk <ryuty77@naver.com>
2025-02-11 23:08:48 +0900

    update profile_pics

--- app/sevices/profile_service.py
+++ app/sevices/profile_service.py
@@ -11,6 +11,10 @@ class ProfileService():
         if not profile:
             raise HTTPException(status_code=404, detail="Profile not found")
         return profile
+    
+    def get_profiles(self, users):
+        #TODO: 팔로우/팔로워 테이블에서 팔로워 리스트(users)를 추출했을 때, 해당하는 프로필들을 가져오기
+        pass
 
     def create_profile(self, profile_data:CreateProfileReq):
         db_profile = Profile(**profile_data.model_dump())
@@ -33,6 +37,8 @@ class ProfileService():
         profile = self.db.exec(select(Profile).where(Profile.user_id == user_id)).first()
         if not profile:
             raise HTTPException(status_code=404, detail="Profile not found")
+        if profile.profile_pic_path:
+            os.remove(profile.profile_pic_path)
         self.db.delete(profile)
         self.db.commit()
         return {"message": "Profile deleted successfully"}
